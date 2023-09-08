6d14735176981217762ddc090fe6c08d7809cc88
sharmarkee <esharmarke2@gmail.com>
2023-09-08 13:16:23 -0500

    input data into seed.js

---
+++ src/data/seed.js
@@ -0,0 +1,33 @@
+export const products = [
+  { 
+ id: 1, 
+ name: 'Air Jordan 1', 
+ price: 200, 
+ size: 14,
+ Avialable: true,
+ image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-Black-Gym-Red-White-2019.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
+
+{
+id: '2', 
+name: 'Yeezy 350 V2', 
+price: 350, 
+size: 11,
+Avialable: true,
+image: 'https://images.stockx.com/images/adidas-Yeezy-Boost-350-V2-Core-Black-Red-2017.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+]
