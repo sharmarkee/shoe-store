63ea5911c3c416a04e847d4f869fdcb98bff5ca5
sharmarkee <esharmarke2@gmail.com>
2023-09-11 15:27:52 -0500

    Cart page created

--- src/data/seed.js
+++ src/data/seed.js
@@ -1,19 +1,50 @@
-export const products = [
+export const shoes = [
   { 
  id: 1, 
  name: 'Air Jordan 1', 
  price: 200, 
  size: 14,
- Avialable: true,
+ Available: true,
  image: 'https://images.stockx.com/images/Air-Jordan-1-Retro-High-Black-Gym-Red-White-2019.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
 
-{
-id: '2', 
+  {
+id: 2, 
 name: 'Yeezy 350 V2', 
 price: 350, 
 size: 11,
-Avialable: true,
+Available: true,
 image: 'https://images.stockx.com/images/adidas-Yeezy-Boost-350-V2-Core-Black-Red-2017.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
+
+  {
+
+id: 3,
+name: 'Air Jordan 4',
+price: 220,
+size: 12,
+Available: false,
+image: 'https://images.stockx.com/images/Air-Jordan-4-Retro-What-The-4.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
+
+  {
+  id: 4,
+  name: 'Air Jordan 11',
+  price: 220,
+  size: 9,
+  Available: true,
+  image: 'https://images.stockx.com/images/Air-Jordan-11-Retro-Low-IE-Space-Jam-2019.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
+  
+  
+  {
+    id: 5,
+    name: 'Nike Air Max 1',
+    price: 140,
+    size: 10,
+    Available: true, 
+    image: 'https://images.stockx.com/images/Nike-Air-Max-1-Susan-Missing-Link.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&trim=color&q=90&dpr=2&updated_at=1566585064' },
+  
+
+
+
+
 
 
 
