14a6578e947af0877141fb4e74a319062e4ba506
sharmarkee <esharmarke2@gmail.com>
2023-09-11 22:19:30 -0500

    Brand,Shoe,Order, and ShoeSchema update

--- models/order.js
+++ models/order.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const shoeItemSchemas = require('./shoeItem');
+const shoeSchema = require('./shoeSchema');
 
-const shoeItemSchema = new Schema({
+
+module.exports = mongoose.model('Order', orderSchema);
+
+const shoeSchema = new Schema({
   qty: { type: Number, default: 1 },
-  item: itemSchema
+  item: shoeSchema
 }, {
   timestamps: true,
   toJSON: {virtuals: true}
 });
 
-shoeItemSchema.virtual('extPrice').get(function() {
+shoeSchema.virtual('extPrice').get(function() {
   return this.qty * this.item.price;
 });
 
 const orderSchema = new Schema({
   // An order belongs to a user
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  // Makes sense to embed an order's line items
-  shoeItems: [shoeItemSchema],
-  // A user's unpaid order is their "cart"
+  user: { type: Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true },
+  
+    shoeItems: [shoeSchema],
+
   isPaid: { type: Boolean, default: false },
 }, {
   timestamps: true,
@@ -38,3 +43,39 @@ orderSchema.virtual('totalQty').get(function () {
 orderSchema.virtual('orderId').get(function () {
   return this.id.slice(-6).toUpperCase();
 });
+
+orderSchema.statics.getCart = async function(userId) {
+  return this.findOneAndUpdate(
+    {user: userId, isPaid: false},
+    {user: userId},
+    {upsert: true, new: true}
+    );
+  };
+
+  orderSchema.methods.addShoeToCart = async function(shoeId) {
+    const cart = this;
+    const shoeItems = cary.shoeItems.find(sjhoeItem => shoeItem.item._id === shoeId);
+
+    if (shoeItem) {
+      shoeItems.qty += 1;
+    } else {
+      const Shoe = mongoose.model('Shoe');
+      const shoe = await Shoe.findyBYID(shoeId);
+      cart.shoeItems.push({item: shoe}); 
+    }
+    return cart.save();
+  };
+
+  orderSchema.methods.setShoeQuantity = async function(shoeId, newQuantity) {
+    cart = this;
+    const shoeItems = cart.shoeItems.find(shoeItems => shoeItems.shoe._id === shoeId);
+
+    if (shoeItem && newQuantity <= 0) {
+      await cart.removeShoe(shoeId);
+    } else if (shoeItem) {
+      shoeItem.quantity = newQuantity;
+    }
+    return cart.save();
+  };
+
+  module.exports = mongoose.model('Order', orderSchema);
